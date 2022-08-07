3d9532fa61225bba6e481625b615559c5973e039
Nishant Jain <nishant.jain.775@gmail.com>
2022-08-07 18:37:40 +0530

    project and dark-mode updation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 ); 
+
+// dark-mode
+const options = {
+  bottom: '64px', // default: '32px'
+  right: '32px', // default: '32px' 
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
